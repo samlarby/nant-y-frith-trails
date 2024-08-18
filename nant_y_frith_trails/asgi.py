7684808e601651cf14a2c12497a6b06cf292f5d5
Sam Larby <samlarby1997@hotmail.com>
2024-08-18 17:53:15 +0000

    initial commit

---
+++ nant_y_frith_trails/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nant_y_frith_trails project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nant_y_frith_trails.settings')
+
+application = get_asgi_application()
---
+++ nant_y_frith_trails/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nant_y_frith_trails project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nant_y_frith_trails.settings')
+
+application = get_wsgi_application()
